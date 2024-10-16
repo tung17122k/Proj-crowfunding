024afbdf6a35334cfb4e7f340cd5f2bcf41cf7fe
tung17122k <sanking1712@gmail.com>
2024-10-16 11:37:52 +0700

    add Dashboard Search details

--- src/layout/LayoutDashboard.js
+++ src/layout/LayoutDashboard.js
@@ -1,10 +1,12 @@
+import Overlay from "component/common/Overlay";
 import DashboardSidebar from "modules/dashboard/DashboardSidebar";
 import DashboardTopbar from "modules/dashboard/DashboardTopbar";
 import React from "react";
 
 const LayoutDashboard = ({ children }) => {
   return (
-    <div className="min-h-screen p-10 bg-lite">
+    <div className="min-h-screen p-10 bg-lite ">
+      <Overlay></Overlay>
       <DashboardTopbar></DashboardTopbar>
       <div className="flex items-start gap-x-10">
         <DashboardSidebar></DashboardSidebar>
